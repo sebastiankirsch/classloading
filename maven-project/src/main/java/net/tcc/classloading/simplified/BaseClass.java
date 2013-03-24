3387606a142e0fe6824eea676c1d386e6e8e0190
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-03-25 00:06:41 +0100

    instantiating the class does the trick

--- maven-project/src/main/java/net/tcc/classloading/simplified/BaseClass.java
+++ maven-project/src/main/java/net/tcc/classloading/simplified/BaseClass.java
@@ -3,14 +3,7 @@ package net.tcc.classloading.simplified;
 public class BaseClass {
 
     static {
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        if (!Thread.currentThread().isInterrupted()) {
-            System.out.println(SubClass.class);
-        }
+        new SubClass();
     }
 
 }
